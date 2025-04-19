fad10befdc3a6483d98cda1c539f16d4c4c38109
KarensArtsy <Karensartsy@gmail.com>
2025-04-18 23:31:36 -0700

    Create 6KNGuN3pbTX_captions.js

---
+++ IST526/Lab04/Lab04_Equity_vs_Equality/SCORM/Lab04_Equity_vs_Equality/story_content/6KNGuN3pbTX_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.97.34031.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:01.696%0D%0AEquality%20vs.%20Equity%0D%0A%0D%0A00:00:01.729%20--%3E%2000:00:02.941%0D%0AWhat%20is%20the%20difference?%0D%0A%0D%0A"}')
