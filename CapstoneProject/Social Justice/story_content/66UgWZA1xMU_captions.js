4dd023ed1f3cebe6699875668d2fb431d744d755
KarensArtsy <Karensartsy@gmail.com>
2025-05-13 14:49:56 -0700

    Final project

--- CapstoneProject/Social Justice/story_content/66UgWZA1xMU_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalLoadJsAsset('story_content/66UgWZA1xMU_captions.js', {"captions":[{"langCode":"en","data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.99.34516.0%0D%0A%0D%0A00:00:00.040%20--%3E%2000:00:02.363%0D%0ABy%20the%20end%20of%20this%20lecture,%20you%20will%20be%20able%20to%0D%0A%0D%0A"}]})
